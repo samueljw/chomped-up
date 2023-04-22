0765c1ede7fc6ecc73fb9a3fc3ea2c244f547afc
Ernest Salim <salim.ernest17@gmail.com>
2023-04-22 15:00:49 -0700

    feat: token

--- client/App.js
+++ client/App.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
+import { useState, useEffect } from 'react';
 import {
     API_KEY,
     AUTH_DOMAIN,
@@ -13,6 +14,7 @@ import UserContext from './src/contexts/UserContext';
 import FirebaseStorageContext from './src/contexts/FireStorageContext';
 import TabScreen from './src/screens/TabScreens';
 import FontWrapper from './src/utils/FontWrapper';
+import { getFromLocal } from './src/utils/AsyncStorage';
 
 const firebaseConfig = {
     apiKey: API_KEY,
@@ -27,10 +29,15 @@ const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
 export default function App() {
-    const value = 'hello';
+    const [token, setToken] = useState('');
+
+    useEffect(async () => {
+        const tokenFromStorage = await getFromLocal('TOKEN');
+        setToken(tokenFromStorage);
+    }, []);
 
     return (
-        <UserContext.Provider value={value}>
+        <UserContext.Provider value={token}>
             <FirebaseStorageContext.Provider value={storage}>
                 <FontWrapper>
                     <TabScreen></TabScreen>
