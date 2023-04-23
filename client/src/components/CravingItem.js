e384a735b9166695b3137cdef6ae9c4e3e7e3798
jesse samuel widjaja <jessewidjaja@gmail.com>
2023-04-22 21:08:53 -0700

    profile

--- client/src/components/CravingItem.js
+++ client/src/components/CravingItem.js
@@ -1,32 +1,47 @@
-import { View, Text, StyleSheet, TouchableOpacity, ImageBackground, Image } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    ImageBackground,
+    Image,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-import { white } from '../../assets/colors';
+import { white } from "../../assets/colors";
+import { useContext, useEffect, useState } from "react";
+import UserContext from "../contexts/UserContext";
 
-const CravingItem = ({ restaurant }) => {
+const CravingItem = ({ navigation, restaurant, id }) => {
+    console.log("user id", id);
+
     return (
-        <TouchableOpacity style={styles.wrapper}>
+        <TouchableOpacity
+            onPress={() => navigation.navigate("Profile", { id })}
+            style={styles.wrapper}>
             <ImageBackground
-                source={require('../../assets/download.jpeg')}
-                resizeMode={'cover'}
+                source={require("../../assets/download.jpeg")}
+                resizeMode={"cover"}
                 imageStyle={{ borderRadius: 10 }}
-                style={styles.backdrop}
-            >
+                style={styles.backdrop}>
                 <LinearGradient
-                    colors={['transparent', 'rgba(0, 0, 0, 0.9)']}
+                    colors={["transparent", "rgba(0, 0, 0, 0.9)"]}
                     style={{
-                        position: 'absolute',
+                        position: "absolute",
                         left: 0,
                         top: 0,
-                        width: '100%',
-                        height: '100%',
+                        width: "100%",
+                        height: "100%",
                         borderBottomLeftRadius: 10,
                         borderBottomRightRadius: 10,
                     }}
                 />
                 <View style={styles.container}>
-                    <Image source={require('../../assets/download.jpeg')} style={styles.profile} />
-                    <Text style={styles.restaurant}>{restaurant}</Text>
+                    <Image
+                        source={require("../../assets/download.jpeg")}
+                        style={styles.profile}
+                    />
+                    <Text style={styles.restaurant}>{restaurant?.title}</Text>
                 </View>
             </ImageBackground>
         </TouchableOpacity>
@@ -36,19 +51,19 @@ const CravingItem = ({ restaurant }) => {
 const styles = StyleSheet.create({
     wrapper: {
         width: 120,
-        height: '100%',
+        height: "100%",
         borderRadius: 20,
         marginRight: 15,
     },
     backdrop: {
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
     },
     container: {
         paddingHorizontal: 10,
         paddingTop: 10,
         paddingBottom: 20,
-        justifyContent: 'space-between',
+        justifyContent: "space-between",
         flex: 1,
     },
     profile: {
@@ -57,11 +72,11 @@ const styles = StyleSheet.create({
         borderColor: white,
         width: 50,
         height: 50,
-        resizeMode: 'cover',
+        resizeMode: "cover",
     },
     restaurant: {
         fontSize: 18,
-        fontFamily: 'Lora_700Bold',
+        fontFamily: "Lora_700Bold",
         color: white,
     },
 });
